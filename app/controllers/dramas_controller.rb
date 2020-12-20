55a34dac38c680df3234acc3570d42b8b7c0ca44
katiethile <lethikatie@gmail.com>
2020-12-20 02:44:26 -0800

    updated all methods in dramas controller

--- app/controllers/dramas_controller.rb
+++ app/controllers/dramas_controller.rb
@@ -4,10 +4,31 @@ class DramasController < ApplicationController
     end 
 
     def new 
-
+        @drama = Drama.new 
     end 
 
     def show 
         @drama = Drama.find(params[:id])
+    end 
+
+    def create 
+        @drama = Drama.new(drama_params)
+        if @drama.save 
+            redirect_to drama_path(@drama)
+    end 
+end 
+
+    private 
+    def drama_params
+        params.require(:drama).permit(
+            :name, :genre,
+            reviews_attributes: [
+                :title,
+                :content,
+                :rating,
+                :user_id,
+                :drama_id,
+              ]
+        )
     end 
 end
